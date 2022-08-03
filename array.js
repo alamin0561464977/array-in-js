b6c68445a256b53a31b46c4e2a3b8e322eb7bbbb
alamin0561464977 <amin23435678901315@gmail.com>
2022-08-04 02:34:28 +0300

    first commit

---
+++ array.js
@@ -0,0 +1,35 @@
+/* 
+---------------------
+ARRAY
+-----------------------
+1. What is the purpose?
+2. How to declare an array in JS
+3. Number of elements in an Array
+4. What is index? 
+5. Find the value of an element by index
+6. Change an element by index
+7. get the index of an element by the value
+8. what does it mean when you get undefined while getting the value of an element by index
+9. How can you add an element to an array at the last position
+10. How can you remove the last element from array
+11. Add an element at the first position of an array
+12. Remove the first element of an array
+*/
+
+
+// 2. How to declare an array in JS
+var array = [23, 45, 67, 49, 18, 70];
+
+// 5. Find the value of an element by index
+// console.log(array)
+
+// 6. Change an element by index
+array[2] = 200;
+array.push(100);
+array.pop();
+array.shift()
+array.unshift(300)
+array.indexOf(45);
+array.length
+array[1]
+console.log(array[1])
